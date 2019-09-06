ec05411581fe5b0f9b636ce26f3b7c8d79fcf316
JustinBrown123 <j-brown123@live.com>
2019-09-05 20:23:28 -0400

    first

---
+++ PredictFuture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PredictFuture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
